49396a94c8cadfc40dc25bfbadb8770427b6f8f8
ArnoldLuich <73598232+ArnoldLuich@users.noreply.github.com>
2020-11-30 11:24:12 +0200

    Update script.js
    java muutmine

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
-function myFunction() {
-	document.getElementById("pealkiri").style.fontSize = "50px"; 
-	document.getElementById("pealkiri").style.color = "white";
-	document.getElementById("pealkiri").style.backgroundColor = "black";        
-	}
+<script>
+	function myFunction() {
+		document.getElementById("pealkiri").style.fontSize = "50px"; 
+		document.getElementById("pealkiri").style.color = "white";
+		document.getElementById("pealkiri").style.backgroundColor = "black";        
+		}
+</script>
