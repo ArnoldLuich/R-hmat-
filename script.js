90db8664b977302aa45e097565d683c36420f886
ArnoldLuich <73598232+ArnoldLuich@users.noreply.github.com>
2020-11-30 11:21:58 +0200

    Update script.js
    java muutmine

--- script.js
+++ script.js
@@ -3,5 +3,4 @@ function myFunction() {
 	document.getElementById("pealkiri").style.fontSize = "50px"; 
 	document.getElementById("pealkiri").style.color = "white";
 	document.getElementById("pealkiri").style.backgroundColor = "black";        
-		}
-<button type="button" onclick="myFunction()">Muuda pealkirja</button>
+	}
