8596e3ad66bb9a56fd72e088ee3ddcaa27249227
ArnoldLuich <73598232+ArnoldLuich@users.noreply.github.com>
2020-11-30 11:31:14 +0200

    Update script.js
    java parandus

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-<script>
-	function myFunction() {
-		document.getElementById("pealkiri").style.fontSize = "50px"; 
-		document.getElementById("pealkiri").style.color = "white";
-		document.getElementById("pealkiri").style.backgroundColor = "black";        
-		}
-		</script>	
+
+function myFunction() {
+	document.getElementById("pealkiri").style.fontSize = "50px"; 
+	document.getElementById("pealkiri").style.color = "white";
+	document.getElementById("pealkiri").style.backgroundColor = "black";        
+	}
