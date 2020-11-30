513d5c6fc38e9d9bc046ee78b5233a7140daca53
ArnoldLuich <73598232+ArnoldLuich@users.noreply.github.com>
2020-11-30 16:30:39 +0200

    Create script2.js
    uus java script

---
+++ script2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	document.getElementById("pealkiri").style.fontSize = "50px"; // kirja suurus
+	document.getElementById("pealkiri").style.color = "red"; // kiri punane
+	document.getElementById("pealkiri").style.backgroundColor = "black";     // taust must      
+	}
