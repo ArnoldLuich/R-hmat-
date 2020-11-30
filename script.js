54fef174f3d863377304aa4806f1f37a3731308f
ArnoldLuich <73598232+ArnoldLuich@users.noreply.github.com>
2020-11-30 16:27:39 +0200

    Update script.js
    kommentaarid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function myFunction() {
-	document.getElementById("pealkiri").style.fontSize = "50px"; 
-	document.getElementById("pealkiri").style.color = "white";
-	document.getElementById("pealkiri").style.backgroundColor = "black";        
+	document.getElementById("pealkiri").style.fontSize = "50px"; // kirja suurus
+	document.getElementById("pealkiri").style.color = "white"; // kiri valge
+	document.getElementById("pealkiri").style.backgroundColor = "black";     // taust must      
 	}
