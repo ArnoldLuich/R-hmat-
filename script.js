4c51b0c7fd2c50b7ed523d5724700fa61a23a871
ArnoldLuich <73598232+ArnoldLuich@users.noreply.github.com>
2020-11-30 11:18:54 +0200

    Create script.js
    java lisamine

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+	document.getElementById("pealkiri").style.fontSize = "50px"; 
+	document.getElementById("pealkiri").style.color = "white";
+	document.getElementById("pealkiri").style.backgroundColor = "black";        
+		}
+<button type="button" onclick="myFunction()">Muuda pealkirja</button>
